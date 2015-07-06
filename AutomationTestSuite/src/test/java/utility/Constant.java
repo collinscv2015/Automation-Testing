6ec7431b37efbc466a81359c54c35db8a74182b5
collinscv2015 <collins.obasuyi@smartfocus.com>
2015-07-06 10:17:42 +0100

    Test Dev Brance

---
+++ AutomationTestSuite/src/test/java/utility/Constant.java
@@ -0,0 +1,14 @@
+package utility;
+
+public class Constant {
+	 
+    public static final String Name = "Automation Testing";
+    public static final String Descriptions = "Automation Testing Descriptions";
+    public static final int Width = 700;
+    public static final int Height = 700;
+
+
+    
+    
+    
+}
